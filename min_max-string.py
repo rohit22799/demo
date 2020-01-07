180cb8847225faeb3fd6bec36df9ce20b22353a7
Hiren Jungi <hiren@techreceptives.com>
2020-01-07 13:16:22 +0530

    Now Take Input From user for Finding max and min string

--- min_max-string.py
+++ min_max-string.py
@@ -1,8 +1,10 @@
-l= ['hi','bye','howru']
+l = input().split(' ')
 c = []
 def longone(l):
     for i in l:
         c.append(len(i))
-        temp = c.index(max(c))
+    temp = c.index(max(c))
+    temp_min = c.index(min(c))
     print(l[temp])
+    print(l[temp_min])
 i = longone(l)
